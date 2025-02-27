b8e3dbd131f0d4d6ce5dc1a95e89a4effa4cc3b7
Mridushisrivastava <mridushisrivastava486@gmail.com>
2025-02-27 16:03:30 +0530

    CALCULATOR

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-let nav=document.querySelector('nav');
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-window.addEventListener('scroll',()=>{
-    nav.classList.toggle('naWinScroll',window,scrollY>0)
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
 })
